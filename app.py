c649922cf366c26282bd06e176bebeceb2c9b850
Chris <chris@Chriss-MBP.grandenetworks.net>
2019-12-09 17:44:52 -0600

    adding template

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask_cors import CORS
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
-    return "HELLO WORLD!!! My name is.....CHRIS"
+    return render_template('index.html')
+    # return "HELLO WORLD!!! My name is.....CHRIS"
+
+@app.route('/leaflet')
+def leaflet():
+    return "This will be my Leaflet map!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8000)
