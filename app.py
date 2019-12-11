51bcbb41bd248ad29cbd2e01b7a92d33108b36ed
Chris <chris@Chriss-MBP.grandenetworks.net>
2019-12-10 18:03:00 -0600

    first trial map push

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask_cors import CORS
 from flask import Flask, render_template
+from folium_maps import return_map
 
 app = Flask(__name__)
 CORS(app)
@@ -11,8 +12,9 @@ def index():
 
 @app.route('/leaflet')
 def leaflet():
-    return "This will be my Leaflet map!"
-
+    
+    map = return_map()
+    return map
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8000)
