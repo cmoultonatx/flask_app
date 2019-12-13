beedff3bc0227df45db4141b8b2dd7ecbe288533
Chris <chris@Chriss-MBP.grandenetworks.net>
2019-12-13 17:07:03 -0600

    somd;lkgahjw

--- app/folium_maps.py
+++ app/folium_maps.py
@@ -5,5 +5,5 @@ def return_map(coords):
     inv_coords = [coords[1],coords[0]]
     map = folium.Map(location=inv_coords,zoom_start=5)
     geojson = get_geojson(coords)
-    layer = folium.GeoJson(geojson).add_to(map)
+    # layer = folium.GeoJson(geojson).add_to(map)
     return map
